2874c96b139ceb7847e27fd968a6ea92884a4dfe
davidjcmar <davidjcmar@gmail.com>
2015-11-18 22:01:08 -0600

    done?

--- server.c
+++ server.c
@@ -102,6 +102,7 @@ int main (void)
 		remain_byte = sizeof tcp_h;
 		while (send_byte < remain_byte)
 		{
+			printf ("loops\n");
 			n = send(sock_client, &tcp_h + send_byte, remain_byte, 0);
 			if (n==-1)
 				break;
@@ -126,6 +127,7 @@ int main (void)
 	remain_byte = sizeof tcp_h;
 	while (send_byte < remain_byte)
 	{
+		printf ("loops\n");
 		n = send(sock_client, &tcp_h + send_byte, remain_byte, 0);
 		if (n==-1)
 			break;
