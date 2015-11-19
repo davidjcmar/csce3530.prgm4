5f62048a0d35915b1b843103cccaab4db2fdc950
davidjcmar <davidjcmar@gmail.com>
2015-11-18 19:03:52 -0600

    handshake complete

--- header.h
+++ header.h
@@ -10,6 +10,8 @@
 
 #define PORT_NO 56565
 #define SERV_IP "129.120.151.94"
+#define CLIENT_IP "129.120.151.95"
+#define DATA_LEN 128
 
 struct tcp_head {
 	unsigned short int source_port, dest_port;
@@ -17,6 +19,7 @@ struct tcp_head {
 	unsigned short int flags, window;
 	unsigned short int chksum, urg_ptr;
 	unsigned int options;
+	char data[128];
 };
 
 int send_loop (int, char*, int*);
