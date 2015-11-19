10bd1c01a4e0c1cfcf96c94e6d97d708de0c6885
davidjcmar <davidjcmar@gmail.com>
2015-11-19 15:54:19 -0600

    testing

--- server.c
+++ server.c
@@ -101,7 +101,7 @@ int main (void)
 		memset(buffer, '\0', DATA_LEN);
 		send_byte = 0;
 		remain_byte = sizeof tcp_h;
-		printf ("%d %d 0x%04x 0x%04x \n0x%02x 0x%02x 0x%02x 0x%02x 0x%04x\n", \
+		printf ("YO:\n%d %d 0x%04x 0x%04x \n0x%02x 0x%02x 0x%02x 0x%02x 0x%04x\n\n\n", \
 			tcp_h.source_port, tcp_h.dest_port, tcp_h.seq_num,\
 			tcp_h.ack_num, tcp_h.flags, tcp_h.window, tcp_h.chksum, tcp_h.urg_ptr, tcp_h.options);
 		while (send_byte < remain_byte)
